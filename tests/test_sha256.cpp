7bb8c84c65a4064b0d99aa3838dabdda16c4cfe5
Tabatskyi <mark.tabatskyi@gmail.com>
2026-05-01 19:31:13 +0300

    fixed K order

--- tests/test_sha256.cpp
+++ tests/test_sha256.cpp
@@ -22,7 +22,7 @@ TEST(SHA256, EmptyString) {
     sha256_update(&ctx, nullptr, 0);
     sha256_final(&ctx, hash);
 
-    EXPECT_EQ(to_hex(hash), "6608ea9c65671c0722305908a097c5b87e6f4c9ee62a1cbaf178d1b7f7a22e40");
+    EXPECT_EQ(to_hex(hash), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
 }
 
 TEST(SHA256, SingleBlock) {
@@ -33,7 +33,7 @@ TEST(SHA256, SingleBlock) {
     sha256_update(&ctx, reinterpret_cast<const uint8_t*>(data), 3);
     sha256_final(&ctx, hash);
 
-    EXPECT_EQ(to_hex(hash), "c1e1a865fffb17406d5537410a4351868468053c0bc89bca3fae0401a68ab5cf");
+    EXPECT_EQ(to_hex(hash), "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
 }
 
 TEST(SHA256, MultiBlock) {
@@ -44,5 +44,5 @@ TEST(SHA256, MultiBlock) {
     sha256_update(&ctx, reinterpret_cast<const uint8_t*>(data), 56);
     sha256_final(&ctx, hash);
 
-    EXPECT_EQ(to_hex(hash), "140ffc42473dc01b76507da76b78b10fab387eaa411936184503d452a2adab96");
+    EXPECT_EQ(to_hex(hash), "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1");
 }
